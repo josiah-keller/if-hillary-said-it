f9a56f69b5b512d8c15d4e7cbbfa0abdd7cff4b0
Josiah Keller <josiahbkeller@gmail.com>
2018-05-16 01:54:09 -0400

    Switch to puppeteer
    PhantomJS is abandoned, a fact that its maintainers didn't see fit to disclose except in an obscure GitHub issue.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Twitter = require("twitter");
-const { spawn } = require("child_process");
 const fs = require("fs");
+const path = require("path");
+const puppeteer = require("puppeteer");
 const twitterConfig = require("./config");
 
 const CYCLE_INTERVAL = 5 * 60 * 1000; // 5 minutes
@@ -90,7 +91,7 @@ function checkTrumpTweets() {
 function processTweet(tweet) {
     captureScreenshot(tweet).then(image => {
         twitter.post("media/upload", { media: image }).then(media => {
-            twitter.post("statuses/update",  {
+            twitter.post("statuses/update", {
                 status: MESSAGES[Math.floor(Math.random() * MESSAGES.length)],
                 media_ids: media.media_id_string,
             }).then(tweet => {
@@ -102,35 +103,66 @@ function processTweet(tweet) {
             console.error("FATAL: couldn't upload media:", err);
         });
     }).catch(err => {
-        console.error("FATAL: couldn't capture screenshot");
+        console.error("FATAL: couldn't capture screenshot", err);
     });
 }
 
 function captureScreenshot(tweet) {
     return new Promise((resolve, reject) => {
-        let child = spawn("./phantomjs", ["render-tweet.js"]);
-
-        let output = null;
-
-        child.stdin.write(JSON.stringify(tweet));
-        child.stdin.end();
-        child.stdout.on("data", (data) => {
-            if (! output) {
-                output = data;
-            } else {
-                output = Buffer.concat([output, data]);
-            }
-        });
-        child.stderr.on("data", (data) => {
-            console.error(`FATAL: renderer sent to stderr: ${data}`);
-            return reject();
-        });
-        child.on("close", (code) => {
-            if (code !== 0) {
-                console.error("FATAL: renderer exited with code", code);  
-                return reject(code);  
-            }
-            resolve(output);
+        puppeteer.launch().then(async browser => {
+            let page = await browser.newPage();
+            await page.goto("file:///" + path.resolve(__dirname, "tweet-renderer/tweet.html"));
+            let clipRect = await page.evaluate(tweet => {
+                function parseTweetText(tweet) {
+                    var tweetText;
+                    if (tweet.truncated) {
+                        if (tweet.extended_tweet) {
+                            tweetText = tweet.extended_tweet.full_text;
+                        }
+                    }
+                    if (! tweetText) {
+                        tweetText = tweet.full_text || tweet.text;
+                    }
+                    return tweetText;
+                }
+        
+                var $ = function(id) { return document.getElementById(id); };
+                var clipRect = {};
+        
+                var tweetText = parseTweetText(tweet);
+                if (! tweet.quoted_status) {
+                    $("render-quote-tweet").style.display = "none";
+                } else {
+                    $("render-quote-tweet-name").textContent = tweet.quoted_status.user.name;
+                    $("render-quote-tweet-username").textContent = tweet.quoted_status.user.screen_name;
+                    $("render-quote-tweet-verified").style.display = tweet.quoted_status.user.verified ? "" : "none";
+                    $("render-quote-tweet-text").textContent = parseTweetText(tweet.quoted_status);
+                    
+                    // Slightly hacky way to get rid of the t.co URL for quote tweets
+                    for (var i=0; i<tweet.entities.urls.length; i++) {
+                        var entity = tweet.entities.urls[i];
+                        if (entity.expanded_url.replace(
+                            new RegExp("^https://twitter.com/" + tweet.quoted_status.user.screen_name + "/status/(\\d+)"), "$1")
+                            == tweet.quoted_status.id_str) {
+                                tweetText = tweetText.replace(entity.url, "");
+                            }
+                    }
+                }
+                $("render-tweet-text").textContent = tweetText;
+        
+                var rect = $("render-tweet").getBoundingClientRect();
+                clipRect.x = rect.left;
+                clipRect.y = rect.top;
+                clipRect.width = rect.width;
+                clipRect.height = rect.height;
+        
+                return Promise.resolve(clipRect);
+            }, tweet)
+            let image = await page.screenshot({
+                clip: clipRect,
+            });
+            browser.close();
+            return resolve(image);
         });
     });
 }
